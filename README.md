ca7461000c4297807bcedb896a906f6b9356fe98
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-02-01 23:02:40 -0500

    particularized some of the functions, cleaned up some of the code

--- README.md
+++ README.md
@@ -4,7 +4,15 @@
 ---
 
 ## Description
+This is a simple application to organize someone's work day. You can schedule various events by clicking on a textbox and entering text. Hitting enter or clicking the save button will save your inputs to local storage, and you will still see them if you refresh the page or close it and open it later. To clear the page from anything that the user entered, the "clear schedule" button can be selected at any time.
 
 ## Usage
 
+
+
 ## Features
+`function populatePage()` is a complicated function that really ought to be broken down into subfunctions.
+
+
+## Outstanding Updates
+- [] break down the populatePage() function into discrete subfunctions with its own parameters
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 today = moment();
 var scheduleStartTime = 9;
 var scheduleEndTime = 16;
+var localStorageAvailable = localStorage.getItem("savedSchedule") != null; //boolean that indicates whether content has been saved to the local storage
 
+//set up our page
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY")); //set the date in the header
 populatePage();
 
@@ -11,11 +13,11 @@ function populatePage() {
     let hour = moment(0).hour(i).format("h:mm A");
 
     let newTimeBlockEl = $("#template-time-block").clone(); //cloning a new time-block element to populate to our page, modified/customized below
-    newTimeBlockEl.attr("style", "display: flex"); //our template time-block is hidden, and we want to see our clones
+    newTimeBlockEl.attr("style", "display: flex"); //our template time-block is hidden, but we want to see our clones
     newTimeBlockEl.attr("id", "time-block-"+i);
 
     // Set the hour element in our new time-block
-    newTimeBlockEl.children(".hour").text(hour); 
+    
 
     // Set the textarea element in our new time-block
     let textareaPlaceholder = hour + " \- " + (moment(0).hour(i+1).format("h:mm A"));
@@ -23,29 +25,37 @@ function populatePage() {
     newTimeBlockEl.children("textarea").attr("id", "schedule-item-"+i);
     
     //assign the appropriate class depending on whether the time block is in the future, past, or present for today
-    if (moment().hour()>i) {
-        newTimeBlockEl.children("textarea").attr("class", "past description col-9 border");
-    }
-    else if (moment().hour()==i) {
-        newTimeBlockEl.children("textarea").attr("class", "present description col-9 border");
-    }
-    else {
-        newTimeBlockEl.children("textarea").attr("class", "future description col-9 border");
-    }
+    assignElementClasses (newTimeBlockEl, i);
+    setElementText(newTimeBlockEl.children(".hour"), hour);
     
-    if(localStorage.getItem("savedSchedule")){ // If available, populate any previously saved entries for this time slot from local storage
-        let savedSchedule = JSON.parse(localStorage.getItem("savedSchedule"));
-        for (let j=0; j<=scheduleEndTime-scheduleStartTime; j++){
-            if (j+scheduleStartTime==i) {
-                newTimeBlockEl.children("textarea").val(savedSchedule[j].item);
-            }
-        }
-
+    if(localStorageAvailable){ // If local storage is available, populate any previously saved entries for this time slot from local storage
+        writeFromStorage(newTimeBlockEl.children("textarea"), "savedSchedule", i);
     }
 
     //append our new time-block to the page
     $("#time-block-container").append(newTimeBlockEl); 
     }
+}
+
+function writeFromStorage (element, localStorageID, index) { //writes whatever is in the local storage object at a given index to the provided element
+    let savedSchedule = JSON.parse(localStorage.getItem(localStorageID));
+    element.val(savedSchedule[index-scheduleStartTime].item);
+}
+
+function assignElementClasses (element, i) { //checks to see if a timeblock is for a past, present, or future timeblock and sets the class accordingly
+    if (moment().hour()>i) {
+        element.children("textarea").addClass("past");
+    }
+    else if (moment().hour()==i) {
+        element.children("textarea").addClass("present");
+    }
+    else {
+        element.children("textarea").addClass("future");
+    }
+}
+
+function setElementText (element, text) {
+    element.text(text); 
 }
 
 function savePage () {
@@ -69,7 +79,7 @@ function clearStorage () {
     location.reload();
 }
 
-// Saving user inputs:
+// U S E R     I N P U T S
 $("textarea").on("keypress", function(event){ //if the user hits "Enter", save what's on the page
     if (event.key === "Enter") { 
         event.preventDefault(); //makes sure the "Enter" button does not take the user to a new line but just saves their work
@@ -78,7 +88,7 @@ $("textarea").on("keypress", function(event){ //if the user hits "Enter", save w
 });
 
 $(".saveBtn").on("click", savePage); //if the user hits the save button, saves what's on the page
-$("#clear-storage-btn").on("click", clearStorage);
+$("#clear-storage-btn").on("click", clearStorage); //if the user selects the "clear schedule" button, clears the local storage
 
 
 
